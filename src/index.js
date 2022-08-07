0ed233ae599d436ca6d2276b8b29984c32c8446b
tkoenig97 <tylerskoenig@gmail.com>
2022-08-07 14:21:18 -0500

    Added props to CommentDetail

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { faker } from "@faker-js/faker";
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <CommentDetail author="Sam"/>
+            <CommentDetail author="Alex"/>
+            <CommentDetail author="Jane"/>
         </div>
     ); 
 };
