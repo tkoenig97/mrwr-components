c3accd291caf2d3c36b594d68879dd308517e06f
tkoenig97 <tylerskoenig@gmail.com>
2022-08-07 14:50:22 -0500

    Added CommentDetail as ApprovalCard child

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
-            <CommentDetail author="Sam" 
-                           timeAgo="Today at 4:45PM" 
-                           text="Nice Blog Post!"
-            />
+            <ApprovalCard>
+                <CommentDetail author="Sam" 
+                            timeAgo="Today at 4:45PM" 
+                            text="Nice Blog Post!"
+                />
+            </ApprovalCard>
             <CommentDetail author="Alex" 
                            timeAgo="Today at 2:00AM" 
                            text="Fat Stu"
