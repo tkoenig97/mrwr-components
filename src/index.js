c10a51f3277b36be6da99bbd0ee48261f86d6ad7
tkoenig97 <tylerskoenig@gmail.com>
2022-08-07 13:57:08 -0500

    Duplicating a Single Component

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { faker } from "@faker-js/faker";
 
 const App = () => {
     return (
-        <div className="ui container comments">
+        <><><div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt='avatar' src={faker.image.image()}/>
+                    <img alt='avatar' src={faker.image.avatar()} />
                 </a>
                 <div className="content">
                     <a href="/" className="author">
@@ -19,8 +19,38 @@ const App = () => {
                     <div className="text">Nice blog post!</div>
                 </div>
             </div>
-        </div>
-    );
+        </div><div className="ui container comments">
+                <div className="comment">
+                    <a href="/" className="avatar">
+                        <img alt='avatar' src={faker.image.avatar()} />
+                    </a>
+                    <div className="content">
+                        <a href="/" className="author">
+                            Sam
+                        </a>
+                        <div className="metadata">
+                            <span className="date">Today at 6:00PM</span>
+                        </div>
+                        <div className="text">Nice blog post!</div>
+                    </div>
+                </div>
+            </div></><div className="ui container comments">
+                <div className="comment">
+                    <a href="/" className="avatar">
+                        <img alt='avatar' src={faker.image.avatar()} />
+                    </a>
+                    <div className="content">
+                        <a href="/" className="author">
+                            Sam
+                        </a>
+                        <div className="metadata">
+                            <span className="date">Today at 6:00PM</span>
+                        </div>
+                        <div className="text">Nice blog post!</div>
+                    </div>
+                </div>
+            </div></>
+    ); 
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
