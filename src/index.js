6027fc4ae94427d1b71644f691497f0f3a3580c9
tkoenig97 <tylerskoenig@gmail.com>
2022-08-07 14:46:17 -0500

    Added ApprovalCard component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" text="Nice Blog Post!"/>
-            <CommentDetail author="Alex" timeAgo="Today at 2:00AM" text="Yo yo yo"/>
-            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" text="Cockerspaniel"/>
+            <ApprovalCard />
+            <CommentDetail author="Sam" 
+                           timeAgo="Today at 4:45PM" 
+                           text="Nice Blog Post!"
+            />
+            <CommentDetail author="Alex" 
+                           timeAgo="Today at 2:00AM" 
+                           text="Fat Stu"
+            />
+            <CommentDetail author="Jane" 
+                           timeAgo="Yesterday at 5:00PM" 
+                           text="Cockerspaniel"
+            />
         </div>
     ); 
 };
